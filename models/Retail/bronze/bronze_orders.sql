dc4e5ea1e4683969dbc7d2627fd02521a10142bd
Alexey Serdtse <ranger_lex@proton.me>
2025-08-06 19:22:46 +0000

    Added incremental materialization

--- models/Retail/bronze/bronze_orders.sql
+++ models/Retail/bronze/bronze_orders.sql
@@ -1,4 +1,13 @@
-select * 
-  from {{source('landing', 'orders')}} 
-  
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'id'
+    )
+}}
 
+select * 
+  from {{ source('landing', 'orders') }} 
+  
+{%- if is_incremental() %}
+    where created_at > (select coalesce(max(created_at),'1900-01-01') from {{ this }})
+{%- endif %}
